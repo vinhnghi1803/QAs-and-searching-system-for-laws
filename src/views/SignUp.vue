e9321ecedc973bd2fcd56194fc30e2f91a5ba6f9
vinhnghi1803 <105730638+vinhnghi1803@users.noreply.github.com>
2024-06-25 16:52:11 +0700

    Update SignUp.vue

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -13,7 +13,7 @@
     <v-row justify="center">
       <v-col cols="12" sm="8" md="6" lg="4">
         <v-form ref="signupData" @submit.prevent="submitForm">
-          <v-card rounded="xl" elevation="10" class="pa-7" max-width="500">
+          <v-card rounded="xl" elevation="10" class="pa-7 mx-auto" max-width="500">
             <v-card-title class="justify-center text-h4 mb-4 font-weight-black blue--text text--lighten-1">{{ title }}</v-card-title>
 
             <v-text-field v-model="signupData.fullName" label="Full name" outlined :rules="fullNameRules" required rounded color="grey darken-2">
